be5f17b509da0c716cb9e81767cf74206b5e207b
Alfie Foster <bonzi@lordbonzi.pro>
2018-01-10 20:54:35 +0000

    Update lastfm.js

--- assets/js/lastfm.js
+++ assets/js/lastfm.js
@@ -3,7 +3,7 @@
 
 // Code taken from https://codepen.io/XanderStrike/pen/zfExv
 
-$.getJSON('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=LordBonzi&api_key=f96f4d2f1ed290cd64d8fb3e816f2f18&format=json', function(data) {
+$.getJSON('https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=LordBonzi&api_key=f96f4d2f1ed290cd64d8fb3e816f2f18&format=json', function(data) {
     var track = data.recenttracks.track[0]
     $('#lastfm').html(track.artist["#text"] + " - " + track.name)
   });
